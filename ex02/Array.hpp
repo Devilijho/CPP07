3e3a3f60c7de9535d53f8ce7ebabac77f0f93648
Santiago Fuentes Llanos <safuente@f6r3s7.clusters.42paris.fr>
2025-08-27 15:48:37 +0200

    still working one x02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,14 +1,53 @@
 #pragma once
 
-#include "Array.tpp"
+#include <cstddef>
+#include <ctime>
+#include <time.h>
+#include <iostream>
+#include <cstdlib>
 
-template <typename T> class Array
+template <typename TypeArray> class Array
 {
 	public:
-		Array();
-		Array(size_t n);
-		Array(const Array &other);
-		Array *operator=(const Array &other);
-		T	operator[](size_t pos);
-		size_t size();
+		TypeArray *array;
+		Array()
+		{
+			array = NULL;
+		}
+		Array(size_t n)
+		{
+			TypeArray new_array[n];
+			array = new_array;
+		}
+		Array(const Array &other)
+		{
+			(void)other;
+		}
+		Array *operator=(const Array &other)
+		{
+			if (this != other)
+			{
+				this = other;
+			}
+			return (this);
+		}
+		TypeArray	*operator[](size_t pos)
+		{
+			if (pos >= size())
+			{
+				throw std::exception();
+			}
+			return array[pos];
+		}
+		size_t size()
+		{
+			TypeArray *ptr = array;
+			size_t size = 0;
+			while (ptr != NULL)
+			{
+				ptr++;
+				size++;
+			}
+			return (size);
+		}
 };
