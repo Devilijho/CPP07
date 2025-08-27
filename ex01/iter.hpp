294ea5cb40c6d5119b7b9267954cfc42aa4deb1d
Santiago Fuentes <santi.fuentes.llanos@gmail.com>
2025-08-27 08:45:56 +0200

    uay

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,9 +3,9 @@
 #include <string>
 #include <iostream>
 
-template<typename T>
+template<typename T, typename T2>
 
-void iter(T* addr, size_t lenght, void (*f)(T &))
+void iter(T* addr, size_t lenght, T2 f)
 {
 	for (size_t i = 0; i < lenght; i++)
 	{
