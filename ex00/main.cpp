e729bfcaba4cba8136a92dc5f217c003114eb38c
Santiago Fuentes Llanos <safuente@f2r12s16.clusters.42paris.fr>
2025-09-27 14:43:51 +0200

    ex00 good

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,17 +2,29 @@
 
 int main( void )
 {
+	std::cout << "INT TEST" << std::endl;
 	int a = 2;
 	int b = 3;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::cout << "STRING TEST" << std::endl;
+	std::string c = "Nacho";
+	std::string d = "Chuchuwa";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "BOOL TEST" << std::endl;
+	bool e = true;
+	bool f = false;
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
 	return 0;
 }
