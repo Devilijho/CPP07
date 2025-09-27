040336d9ae4c9c932bfb3804a47300c7e1aac203
Santiago Fuentes Llanos <safuente@f2r12s16.clusters.42paris.fr>
2025-09-27 14:58:15 +0200

    all goo

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -7,6 +7,8 @@ template<typename T1, typename T2>
 
 void iter(T1* addr, size_t lenght, T2 f)
 {
+	if (!addr || !f)
+		return ;
 	for (size_t i = 0; i < lenght; i++)
 	{
 		f(addr[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,27 +15,24 @@ void fun(char &c)
 int main()
 {
 	{
+		std::cout << "______TEST ON STRING______" << std::endl;
 		std::string str = "gnk@";
 		char *ptr = &str[0];
-		std::cout << ptr << std::endl;
+		std::cout << "string before iter-> " << ptr << std::endl;
 		::iter(ptr, strlen(ptr), &fun);
-		std::cout << ptr << std::endl;
+		std::cout << "string after iter-> "  << ptr << std::endl;
 	}
 	{
+		std::cout << "______TEST ON INTs______" << std::endl;
 		int n[5] = {1, 3, 5, 7 ,9};
+		std::cout << "int array before iter-> ";
 		for (int i = 0; i < 5; i++)
 			std::cout << n[i];
 		::iter(n, 5, &nuf);
 		std::cout << std::endl;
+		std::cout << "int array after iter-> ";
 		for (int i = 0; i < 5; i++)
 			std::cout << n[i];
 		std::cout << std::endl;
-	}
-	{
-		std::string str = "gnk@";
-		char *ptr = &str[0];
-		std::cout << ptr << std::endl;
-		::iter(ptr, strlen(ptr), &fun);
-		std::cout << ptr << std::endl;
 	}
 }
